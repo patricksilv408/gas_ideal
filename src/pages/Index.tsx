182bc7b8d0f31369d4965a99e878125b21e138be
[dyad] <patrick.santosilv@gmail.com>
2025-11-14 23:45:47 -0300

    [dyad] Removed benefits section - wrote 1 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,4 @@
 import HeroSection from "@/components/hero-section";
-import BenefitsSection from "@/components/benefits-section";
 import ProductSection from "@/components/product-section";
 import GoogleReviewsSection from "@/components/google-reviews-section";
 import Footer from "@/components/footer";
@@ -9,7 +8,6 @@ const Index = () => {
   return (
     <div className="min-h-screen">
       <HeroSection />
-      <BenefitsSection />
       <ProductSection />
       <GoogleReviewsSection />
       <Footer />
